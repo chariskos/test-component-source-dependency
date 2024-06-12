62e71689c6afb99d698d369e71cdad2a310856ee
petros-chariskos <petros.chariskos@caspeco.se>
2024-06-12 12:54:06 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
